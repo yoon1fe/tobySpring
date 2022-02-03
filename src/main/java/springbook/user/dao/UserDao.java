028d3c54585b730540904c71b5f821f92ec8d563
yoon1fe <yoon1fe@naver.com>
2022-02-04 00:54:20 +0900

    3장
    - ~3.2

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -15,74 +15,72 @@ public class UserDao {
   private DataSource dataSource;
 
   public void add(User user) throws SQLException {
-    Connection conn = dataSource.getConnection();
 
-    PreparedStatement ps = conn.prepareStatement("insert into users(id, name, password) values(?,?,?)");
-    ps.setString(1, user.getId());
-    ps.setString(2, user.getName());
-    ps.setString(3, user.getPassword());
+    try (
+        Connection conn = dataSource.getConnection();
+        PreparedStatement ps = conn.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+    ) {
+      ps.setString(1, user.getId());
+      ps.setString(2, user.getName());
+      ps.setString(3, user.getPassword());
 
-    ps.executeUpdate();
+      ps.executeUpdate();
+    }
 
-    ps.close();
-    conn.close();
   }
 
 
   public User get(String id) throws SQLException {
-    Connection conn = dataSource.getConnection();
+    try (
+        Connection conn = dataSource.getConnection();
+        PreparedStatement ps = conn.prepareStatement("select * from users where id = ?");
+    ) {
+      ps.setString(1, id);
+      try (
+          ResultSet rs = ps.executeQuery();
+      ) {
+        User user = null;
+        if (rs.next()) {
+          user = User.builder()
+              .id(rs.getString("id"))
+              .name(rs.getString("name"))
+              .password(rs.getString("password"))
+              .build();
+        }
 
-    PreparedStatement ps = conn.prepareStatement("select * from users where id = ?");
-    ps.setString(1, id);
+        if (user == null) {
+          throw new EmptyResultDataAccessException(1);
+        }
 
-    ResultSet rs = ps.executeQuery();
-
-    User user = null;
-    if (rs.next()) {
-      user = User.builder()
-          .id(rs.getString("id"))
-          .name(rs.getString("name"))
-          .password(rs.getString("password"))
-          .build();
+        return user;
+      }
     }
-
-    rs.close();
-    ps.close();
-    conn.close();
-
-    if (user == null) {
-      throw new EmptyResultDataAccessException(1);
-    }
-
-    return user;
   }
 
   public void deleteAll() throws SQLException {
-    Connection conn = dataSource.getConnection();
-
-    PreparedStatement ps = conn.prepareStatement("delete from users");
-
-    ps.executeUpdate();
-
-    ps.close();
-    conn.close();
+    StatementStrategy strategy = new DeleteAllStatement();
+    jdbcContextWithStatementStrategy(strategy);
   }
 
   public int getCount() throws SQLException {
-    Connection conn = dataSource.getConnection();
+    try (
+        Connection conn = dataSource.getConnection();
+        PreparedStatement ps = conn.prepareStatement("select count(*) from users");
+        ResultSet rs = ps.executeQuery()
+    ) {
+      rs.next();
 
-    PreparedStatement ps = conn.prepareStatement("select count(*) from users");
+      return rs.getInt(1);
+    }
+  }
 
-    ResultSet rs = ps.executeQuery();
-
-    rs.next();
-    int count = rs.getInt(1);
-
-    rs.close();
-    ps.close();
-    conn.close();
-
-    return count;
+  public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
+    try (
+        Connection conn = dataSource.getConnection();
+        PreparedStatement ps = stmt.makePreparedStatement(conn);
+    ) {
+      ps.executeUpdate();
+    }
   }
 
 }
